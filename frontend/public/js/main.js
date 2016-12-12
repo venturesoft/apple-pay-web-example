2329d478c068b2c036dc2bcb17990d121e14ec9b
8legd <git@8legd.com>
2016-12-12 11:22:46 +0000

    Restructure frontend/backend

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -243,7 +243,7 @@ function shippingOptionChange (request, details, event) {
 
 jQuery(document).ready(function ($) {
 
-	alert("running version 20161212a");
+	alert("running version 20161212d");
 
 	var applePayButtons = document.querySelectorAll('.apple-pay');
 	Array.prototype.forEach.call(applePayButtons, function (button) {
