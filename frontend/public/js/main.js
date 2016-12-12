63886a7fa63319368b2b9532601d48631551a028
8legd <git@8legd.com>
2016-12-12 11:52:31 +0000

    Restructure frontend/backend

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -98,7 +98,7 @@ function validateMerchant (session, event) {
 	postJson('merchant-validate', {
 		validationURL: event.validationURL
 	}).then(function (response) {
-		alert(JSON.stringify(response));
+		//alert(JSON.stringify(response));
 		session.completeMerchantValidation(response);
 	}, function (status) {
 		alert(JSON.stringify(status));
@@ -243,7 +243,7 @@ function shippingOptionChange (request, details, event) {
 
 jQuery(document).ready(function ($) {
 
-	alert("running version 20161212f");
+	alert("running version 20161212g");
 
 	var applePayButtons = document.querySelectorAll('.apple-pay');
 	Array.prototype.forEach.call(applePayButtons, function (button) {
